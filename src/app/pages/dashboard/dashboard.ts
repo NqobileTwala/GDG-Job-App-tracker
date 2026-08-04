16851bdaf35fe90fc36b38b4a9c5e09393fe5c7a
Nqobile Twala <nqobiletwala02@gmail.com>
2026-08-04 23:02:33 +0200

    page functionalies and formatting

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,13 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { JobService } from '../../services/job';
-import { Job } from '../../models/job.model';
+import { Job, JobStatus, JobUpdate } from '../../models/job.model';
 import { UserService } from '../../services/user';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { User } from '../../models/user.model';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [AsyncPipe, FormsModule],
+  imports: [AsyncPipe, FormsModule, DatePipe, MarkdownModule],
   templateUrl: './dashboard.html'
 })
 export class Dashboard {
@@ -15,6 +17,8 @@ export class Dashboard {
   private userService = inject(UserService);
 
   editedJob?: Job;
+  viewedJob?: Job;
+  editedUser?: Promise<User>
 
   jobs = this.jobService.getJobs();
 
@@ -27,12 +31,64 @@ export class Dashboard {
     };
   }
     cancelJobEdit(){
-      this.editedJob = undefined;
+       this.editedJob = undefined;
     }
 
     saveJob(){
       this.jobService.addJob(this.editedJob!);
       this.editedJob = undefined;
       this.jobs = this.jobService.getJobs();
+    }
+
+    editUser(){
+      this.editedUser = this.userService.getUser().then(user => {
+        if(user){
+          return user;
+        } else {
+          return { email: '', cv: ''};
+        }
+      })
+    }
+    cancelUserEdit(){
+      this.editedUser = undefined;
+    }
+
+    saveUser(){
+      this.editedUser?.then(user => {
+        this.userService.updateUser(user);
+        this.editedUser = undefined;
+      })
+      
+    }
+
+    beginJobView(job: Job){
+      this.viewedJob = job;
+    }
+
+    closeJobDetails(){
+      this.viewedJob = undefined;
+    }
+
+    toggleDetails(job: Job){
+      if(this.viewedJob?.id === job.id){
+        this.viewedJob = undefined;
+      } else {
+        this.viewedJob = job;
+      }
+    }
+
+    getLatestJobUpdate(job: Job): JobUpdate{
+      return job.jobUpdates.reduce((latest, current) =>{
+        return current.updatedAt > latest.updatedAt ? current : latest;
+      });
+    }
+
+    async updateJobStatus(job: Job, newStatus: JobStatus){
+      await this.jobService.updateJobStatus(job.id!, newStatus);
+      job.jobUpdates.push({ status: newStatus, updatedAt: new Date() })
+    }
+
+    getStatuses(){
+      return Object.values(JobStatus);
     }
 }
